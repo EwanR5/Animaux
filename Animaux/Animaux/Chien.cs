f7cdece7746a1c54f2917fcdbc723634ea85c344
EwanR5 <91669749+EwanR5@users.noreply.github.com>
2023-09-12 14:53:52 +0200

    Add files via upload

---
+++ Animaux/Animaux/Chien.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animaux
+{
+    internal class Chien
+    {
+        private string _nom;
+        private string _race;
+        private uint _age;
+        private bool _vaccin;
+
+        public Chien(string nom, string race, uint age)
+        {
+            _nom = nom;
+            _race = race;
+            _age = age;
+        }
+
+        public uint Nom
+        {
+            get { return _nom; }
+        }
+        public uint Race
+        {
+            get { return _race; }
+        }
+        public uint Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+        public bool Vaccin
+        {
+            get { return _vaccin; }
+            set { _vaccin = !value; }
+        }
+
+        public string RecupereCaracteristiques()
+        {
+            string chaine = "Nom : " + _nom + " \n Race : " + _race + "\n Age : " + _age "\n Vacciné : " + _vaccin;
+            return chaine;
+        }
+    }
+}
---
+++ Animaux/Animaux/Program.cs
@@ -0,0 +1,38 @@
+﻿using Animaux;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Chien Chien1 = new Chien("Nom1", "Race1", 1, false);
+        Chien Chien2 = new Chien("Nom2", "Race2", 2, false);
+        Chien Chien3 = new Chien("Nom3", "Race3", 3, false);
+
+        /*string nom;
+        string race;
+        uint age;
+        bool vaccin;*/
+
+        Console.WriteLine(Chien1.RecupereCaracteristiques());
+        Console.WriteLine(Chien2.RecupereCaracteristiques());
+        Console.WriteLine(Chien3.RecupereCaracteristiques());
+        Console.ReadLine();
+
+        Chien1.Age = Chien1.Age + 1;
+        Chien2.Age = Chien2.Age + 1;
+        Chien3.Age = Chien3.Age + 1;
+
+        Chien1.Vaccin = Chien1.Vaccin;
+        Chien2.Vaccin = Chien2.Vaccin;
+        Chien3.Vaccin = Chien3.Vaccin;
+
+        Chien[] mesChiens = new Chien[3];
+
+        mesChiens[0] = Chien1;
+        mesChiens[1] = Chien2;
+        mesChiens[2] = Chien3;
+
+        Console.WriteLine(mesChiens);
+        Console.ReadLine();
+    }
+}
